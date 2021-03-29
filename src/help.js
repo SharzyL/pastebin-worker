9b4c338e913749dadccb1bba1353d201afc21722
SharzyL <me@sharzy.in>
2021-03-30 00:55:34 +0800

    fix unexpected empty line in HELP

--- src/help.js
+++ src/help.js
@@ -4,72 +4,58 @@ export const helpHTML = `
 <h1>Pastebin based on Cloudflare workers</h1>
 <h2>Usage</h2>
 <p> Upload a paste </p>
-<pre> <code>
-$ echo "make Cloudflare great again" | curl -F "c=@-" ${BASE_URL}
+<pre><code>$ echo "make Cloudflare great again" | curl -F "c=@-" ${BASE_URL}
 {
   "url": "${BASE_URL}qotL",
   "admin": "${BASE_URL}qotL_yNm3PTBA3+X1jjhdClJ6zyVMkfA=",
   "isPrivate": false
-}%
-</code> </pre>
+}% </code> </pre>
 
 <p>Fetch the paste</p>
-<pre><code>
-$ curl ${BASE_URL}qotL
-make Cloudflare great again
-</code></pre>
+<pre><code>$ curl ${BASE_URL}qotL
+make Cloudflare great again </code></pre>
 
 <p>Delete the paste</p>
-<pre><code>
-$ curl -X DELETE ${BASE_URL}qotL_yNm3PTBA3+X1jjhdClJ6zyVMkfA=
+<pre><code>$ curl -X DELETE ${BASE_URL}qotL_yNm3PTBA3+X1jjhdClJ6zyVMkfA=
 the paste will be deleted in seconds
 
 $ curl ${BASE_URL}qotL
-not found%
-</code></pre>
+not found% </code></pre>
 
 <p>Update the paste</p>
-<pre><code>
-$ echo "make Cloudflare great again and again" | curl -F "c=@-" ${BASE_URL}qotL_yNm3PTBA3+X1jjhdClJ6zyVMkfA=
+<pre><code>$ echo "make Cloudflare great again and again" | curl -F "c=@-" ${BASE_URL}qotL_yNm3PTBA3+X1jjhdClJ6zyVMkfA=
 {
   "url": "${BASE_URL}qotL",
   "admin": "${BASE_URL}qotL_yNm3PTBA3+X1jjhdClJ6zyVMkfA=",
   "isPrivate": false
-}%
-</code></pre>
+}% </code></pre>
 
 <h2>Advanced Usage</h2>
 
 <p>Fetch the paste with syntax highlighting</p>
-<pre><code>
-$ curl ${BASE_URL}qotL?lang=shell
+<pre><code>$ curl ${BASE_URL}qotL?lang=shell
 </code></pre>
 
 <p>Url redirect</p>
-<pre><code>
-$ curl -L ${BASE_URL}u/qotL
+<pre><code>$ curl -L ${BASE_URL}u/qotL
 </code></pre>
 
 <p>Let the paste expire in 120 seconds</p>
-<pre><code>
-$ echo "make Cloudflare great again" | curl -F "c=@-" -F "e=120" ${BASE_URL}
+<pre><code>$ echo "make Cloudflare great again" | curl -F "c=@-" -F "e=120" ${BASE_URL}
 {
   "url": "${BASE_URL}qotL",
   "admin": "${BASE_URL}qotL_yNm3PTBA3+X1jjhdClJ6zyVMkfA=",
   "isPrivate": false,
   "expire": "120"
-}%
-</code></pre>
+}% </code></pre>
 
 <p>Create a paste with longer path name for better privacy</p>
-<pre><code>
-$ echo "make Cloudflare great again" | curl -F "c=@-" -F "p=true" ${BASE_URL}
+<pre><code>$ echo "make Cloudflare great again" | curl -F "c=@-" -F "p=true" ${BASE_URL}
 {
   "url": "${BASE_URL}HaK8PuBqrLi5woH0cbTBi7uN",
   "admin": "${BASE_URL}HaK8PuBqrLi5woH0cbTBi7uN_TWcWYDRL4SscGQ9P9n7tO7Vu6HU=",
   "isPrivate": true,
-}%
-</code></pre>
+}% </code></pre>
 
 <h2>About</h2>
 <p>API design is inspired by <a href='https://fars.ee'>fars.ee</a></p>
