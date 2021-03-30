97a2c140fceb6bf7eda26e3a8a0cf79aa719639b
SharzyL <me@sharzy.in>
2021-03-30 14:38:33 +0800

    prevent injection in highlighting page

--- src/highlight.js
+++ src/highlight.js
@@ -1,16 +1,28 @@
+String.prototype.escapeHtml = function() {
+  const tagsToReplace = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;'
+  };
+  return this.replace(/[&<>]/g, function(tag) {
+    return tagsToReplace[tag] || tag;
+  });
+};
+
 export function makeHighlight(content, lang) {
   return `<!DOCTYPE html>
-<html>
+<html lang='en'>
 <head>
-  <meta charset="utf-8">
-  <link href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css" rel="stylesheet" />
-  <link href="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/line-numbers/prism-line-numbers.css" rel="stylesheet" />
+  <title>Yet another pastebin</title>
+  <meta charset='utf-8'>
+  <link href='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/themes/prism.css' rel='stylesheet' />
+  <link href='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/line-numbers/prism-line-numbers.css' rel='stylesheet' />
 </head>
-<body class="line-numbers">
-<pre><code class="language-${lang}">${content}</code></pre>
-  <script src="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/components/prism-core.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/line-numbers/prism-line-numbers.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/autoloader/prism-autoloader.min.js"></script>
+<body class='line-numbers'>
+<pre><code class='language-${lang.escapeHtml()}'>${content.escapeHtml()}</code></pre>
+  <script src='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/components/prism-core.min.js'></script>
+  <script src='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/line-numbers/prism-line-numbers.min.js'></script>
+  <script src='https://cdn.jsdelivr.net/npm/prismjs@1.23.0/plugins/autoloader/prism-autoloader.min.js'></script>
 </body>
 </html>
 `
