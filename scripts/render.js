8f5242efd246b77f26f998f18fa2fc3099229adf
SharzyL <me@sharzy.in>
2021-08-10 17:44:36 +0800

    render.js: support commit hash

--- scripts/render.js
+++ scripts/render.js
@@ -1,19 +1,28 @@
 import { Liquid } from 'liquidjs'
 import fs from 'fs'
+import { spawnSync } from 'child_process'
 import { ArgumentParser } from 'argparse'
 
 function parseArgs() {
   const parser = ArgumentParser()
   parser.add_argument('-o', '--output', { required: true })
   parser.add_argument('-c', '--config', { required: true })
+  parser.add_argument('-r', '--revision')
   parser.add_argument('file')
   return parser.parse_args()
+}
+
+function getCommitHash() {
+  const stdout = spawnSync('git', ['rev-parse', '--short=6', 'HEAD']).stdout
+  return stdout === null ? null : stdout.toString().trim()
 }
 
 function main() {
   const args = parseArgs()
 
   const conf = JSON.parse(fs.readFileSync(args.config).toString())
+  conf.COMMIT_HASH_6 = getCommitHash()
+
   const engine = new Liquid()
   const rendered = engine.renderFileSync(args.file, conf)
   fs.writeFileSync(args.output, rendered)
