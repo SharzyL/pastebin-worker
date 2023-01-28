f9f13692bd3764480a5708cd6d8196a05baf42bd
Yang Liu <liuyang22@iscas.ac.cn>
2023-01-29 02:44:39 +0800

    bump wrangler

--- scripts/deploy-static.sh
+++ scripts/deploy-static.sh
@@ -7,11 +7,14 @@ declare -a args=('yarn' 'wrangler' 'kv:key' 'put' '--binding=PB')
 if [ "$1" == '--preview' ]; then
 	args+=('--preview')
 	shift
+else
+	args+=('--preview')
+	args+=('false')
 fi
 
 declare -a files=("$@")
 
 for file in "${files[@]}"; do
 	file_base=$(basename "$file")
-	"${args[@]}" "${file_base%.html}" -p "$file"
+	"${args[@]}" "${file_base%.html}" --path "$file"
 done
