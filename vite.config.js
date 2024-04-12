3e2fb4a84980cf4aec05dce49832b48d078e6dcc
SharzyL <me@sharzy.in>
2024-04-12 12:36:39 +0800

    feat: add coverage

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config"
 
 const cfg = defineWorkersConfig({
-  assetsInclude: [ "frontend/*", "doc/*", "test/resources/*" ],
+  assetsInclude: ["frontend/*", "doc/*", "test/resources/*"],
 
   test: {
+    coverage: {
+      provider: "istanbul", // v8 is not supported due for cf workers
+      reporter: ["text", "json", "html"],
+    },
     poolOptions: {
       workers: {
         wrangler: {
-          configPath: "./wrangler.toml"
+          configPath: "./wrangler.toml",
         },
       },
     },
