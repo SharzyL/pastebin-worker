0ffdb5c0ecc7199243071bb30aa278dcb610f32a
SharzyL <me@sharzy.in>
2025-04-27 14:10:41 +0800

    feat[frontend]: add theme

---
+++ frontend/hero-theme.ts
@@ -0,0 +1,16 @@
+import { heroui } from "@heroui/react"
+
+export default heroui({
+  themes: {
+    dark: {
+      colors: {
+        background: "#111111",
+        primary: {
+          DEFAULT: "#BEF264",
+          foreground: "#111111",
+        },
+        focus: "#BEF264",
+      },
+    },
+  },
+})
